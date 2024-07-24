0b1b9cc038896f6b77d93367f7e5c36cf527bc6e
s-taiga <neverbefore351@gmail.com>
2024-07-24 23:27:48 +0900

    セルフレビュー

--- MIL/C05_Elementary_Number_Theory/S02_Induction_and_Recursion.lean
+++ MIL/C05_Elementary_Number_Theory/S02_Induction_and_Recursion.lean
@@ -24,7 +24,7 @@ which are types generated inductively by a given list of
 In Lean, the natural numbers are declared as follows.
 OMIT. -/
 /- TEXT:
-自然数の集合 :math:`\mathbb{N} = \{ 0, 1, 2, \ldots \}` はその性質から数学の基礎において重要なだけでなく、新たな数学的対象の構成においても主要な役割を演じます。Leanの基礎では *帰納型* を宣言することができます。これは *コンストラクタ* のリストによって帰納的に生成される型です。Leanでは自然数は次のように宣言されます。
+自然数の集合 :math:`\mathbb{N} = \{ 0, 1, 2, \ldots \}` はその性質から数学の基礎において重要なだけでなく，新たな数学的対象の構成においても主要な役割を演じます．Leanの基礎では *帰納型* を宣言することができます．これは *コンストラクタ* のリストによって帰納的に生成される型です．Leanでは自然数は次のように宣言されます．
 OMIT: -/
 namespace hidden
 
@@ -48,7 +48,7 @@ representations, but we don't have to worry about the details of that now.)
 
 OMIT. -/
 /- TEXT:
-このライブラリ中の定義は ``#check Nat`` と記述し、識別子 ``Nat`` を ``ctrl+クリック`` することで見つけることができます。このコマンドは ``Nat`` が2つのコンストラクタ ``zero : Nat`` と ``succ : Nat → Nat`` から自由かつ帰納的に生成されるデータ型であることを示しています。もちろん、ライブラリは ``nat`` と ``zero`` に対してそれぞれ ``ℕ`` と ``0`` という記法を導入しています。（数値は二進数表現に変換されますが、今はその詳細を気にする必要はありません。）
+この型はライブラリ中のにて定義されています．その定義は ``#check Nat`` と書いてから，識別子 ``Nat`` を ``ctrl+クリック`` することで見つけることができます． ``inductive`` コマンドは ``Nat`` が2つのコンストラクタ ``zero : Nat`` と ``succ : Nat → Nat`` から自由かつ帰納的に生成されるデータ型であることを示しています．もちろん，ライブラリは ``nat`` と ``zero`` に対してそれぞれ ``ℕ`` と ``0`` という記法を導入しています．（数値は二進数表現に変換されますが，今はその詳細を気にする必要はありません．）
 
 TEXT. -/
 /- OMIT:
@@ -57,7 +57,7 @@ What "freely" means for the working mathematician is that the type
 ``succ`` whose image does not include ``zero``.
 OMIT. -/
 /- TEXT:
-ここで「自由」という言葉は現役の数学者にとって ``Nat`` 型が要素 ``zero`` と ``zero`` をその像にふくまない単射な後続関数 ``succ`` を持つことを指します。
+ここで「自由」という言葉は現役の数学者にとって ``Nat`` 型が要素 ``zero`` と ``zero`` をその像に含まない単射な後続関数 ``succ`` を持つことを指します．
 EXAMPLES: -/
 -- QUOTE:
 example (n : Nat) : n.succ ≠ Nat.zero :=
@@ -75,7 +75,7 @@ This section will show you how to use these.
 
 OMIT. -/
 /- TEXT:
-また現役の数学者にとって「帰納的」という言葉が意味するのは、自然数には帰納法による証明の原理と再帰による定義の原理が備わっているということです。この節ではこれらの使い方を紹介します。
+また現役の数学者にとって「帰納的」という言葉が意味するのは，自然数には帰納法による証明の原理と再帰による定義の原理が備わっているということです．この節ではこれらの使い方を紹介します．
 
 TEXT. -/
 /- OMIT:
@@ -83,7 +83,7 @@ Here is an example of a recursive definition of the factorial
 function.
 OMIT. -/
 /- TEXT:
-以下が再帰的な定義による階乗関数の例です。
+以下が再帰的な定義による階乗関数の例です．
 BOTH: -/
 -- QUOTE:
 def fac : ℕ → ℕ
@@ -100,7 +100,7 @@ These equations hold definitionally, but they can also
 be used manually by giving the name ``fac`` to ``simp`` or ``rw``.
 OMIT. -/
 /- TEXT:
-この構文には慣れが必要です。最初の行に ``:=`` が無いことに注目してください。そして次の2行は再帰的定義の基本ケースと帰納的なステップを展開しています。以下の等式は定義上保持されるため式変形ではLeanが勝手に展開等を行ってくれますが、 ``fac`` を ``simp`` や ``rw`` に与えることで手動で使用することもできます。
+この構文には慣れが必要です．最初の行に ``:=`` が無いことに注目してください．そして次の2行は再帰的定義の基本ケースと帰納的なステップを展開しています．以下の等式は定義上成り立ちますが， ``fac`` を ``simp`` や ``rw`` に与えることで手動で使用することもできます．
 EXAMPLES: -/
 -- QUOTE:
 example : fac 0 = 1 :=
@@ -134,7 +134,7 @@ that the simplifier uses by default.
 
 OMIT. -/
 /- TEXT:
-この階乗関数はMathlibではすでに ``Nat.factorial`` として定義されています。ここでも再度 ``#check Nat.factorial`` と入力し、 ``ctrl+クリック`` することでこの関数の定義にジャンプすることができます。本書では説明のため、例題では ``fac`` を引き続き使用します。 ``Nat.factorial`` の定義の前にある ``@[simp]`` という注釈は、簡約化がデフォルトで使用する恒等式のデータベースにそこで定義した等式を追加することを指示します。
+この階乗関数はMathlibではすでに ``Nat.factorial`` として定義されています．ここでも再度 ``#check Nat.factorial`` と入力し， ``ctrl+クリック`` することでこの関数の定義にジャンプすることができます．本書では説明のため，例題では ``fac`` を引き続き使用します． ``Nat.factorial`` の定義の前にある ``@[simp]`` という注釈は， ``simp`` がデフォルトで使用する等式のデータベースにそこで定義した等式を追加することを指示します．
 
 TEXT. -/
 /- OMIT:
@@ -152,7 +152,7 @@ the assumption for the inductive hypothesis,
 and you can choose whatever names you want for them.
 OMIT. -/
 /- TEXT:
-帰納法の原理は、自然数に関する一般的な命題を証明するには、その命題が0について成り立ち、また自然数 :math:`n` について成り立つときはいつでも :math:`n + 1` についても成り立つことを証明すれば良いというものです。そのため、以下の証明の ``induction' n with n ih`` という行は2つのゴールを生み出します。1つ目は ``0 < fac 0`` を証明することであり、2つ目は ``ih : 0 < fac n`` という仮定から ``0 < fac (n + 1)`` の証明が求められます。 ``with n ih`` というフレーズは帰納法で用いられる仮説の変数と仮定に名前をつけるためのもので、好きな名前をつけることができます。
+帰納法の原理は，自然数に関する一般的な命題を証明するには，その命題が0について成り立ち，また自然数 :math:`n` について成り立つときはいつでも :math:`n + 1` についても成り立つことを証明すれば良いというものです．そのため，以下の証明の ``induction' n with n ih`` という行は2つのゴールを生み出します．1つ目は ``0 < fac 0`` を証明することであり，2つ目は ``ih : 0 < fac n`` という仮定から ``0 < fac (n + 1)`` の証明が求められます． ``with n ih`` というフレーズは帰納法で用いられる仮定の変数と仮定に名前をつけるためのもので，好きな名前をつけることができます．
 EXAMPLES: -/
 -- QUOTE:
 theorem fac_pos (n : ℕ) : 0 < fac n := by
@@ -170,7 +170,7 @@ induction hypothesis.
 Step through the next example to see what is going on.
 OMIT. -/
 /- TEXT:
-``induction`` タクティクは賢いため、帰納変数に依存する帰納仮説を帰納仮説の一部に含めることができます。次の例でこれがなんのことか見てみましょう。
+``induction`` タクティクは賢いため，帰納変数に依存する仮定を帰納仮定の一部に含めることができます．これがどういうことなのか，次の例で見てみましょう．
 EXAMPLES: -/
 -- QUOTE:
 theorem dvd_fac {i n : ℕ} (ipos : 0 < i) (ile : i ≤ n) : i ∣ fac n := by
@@ -192,7 +192,7 @@ so that the remainder of the proof starts with the case
 See if you can complete the argument with a proof by induction.
 OMIT. -/
 /- TEXT:
-次の例では階乗関数の粗い下界を示します。証明のはじめに場合分けをすると簡単になるため、証明の残りは :math:`n = 1` の場合から始まるようにします。帰納法による証明で論証を完成してみましょう。
+次の例では階乗関数の粗い下界を示します．証明のはじめに場合分けをすると簡単になるため，証明の残りは :math:`n = 1` の場合から始まるようにします．帰納法による証明で論証を完成してみましょう．
 BOTH: -/
 -- QUOTE:
 theorem pow_two_le_fac (n : ℕ) : 2 ^ (n - 1) ≤ fac n := by
@@ -226,7 +226,7 @@ notation for finite products.
 
 OMIT. -/
 /- TEXT:
-有限の和や積を含む恒等式を証明するために帰納法がよく用いられます。Mathlibは ``Finset.sum s f`` という式を定義しています。ここで ``s : Finset α`` は ``α`` 型の要素の有限集合であり、 ``f`` は ``α`` で定義される関数です。 ``f`` の余域は零要素を持つ可換で結合的な加法をサポートする任意の型です。 ``Algebra.BigOperators.Basic`` をインポートして ``open BigOperators`` コマンドを実行することで、より示唆的な表記である ``∑ x in s, f x`` を使うことができます。もちろん有限積についても同様の操作と表記法があります。
+有限の和や積を含む等式を証明するために帰納法がよく用いられます．Mathlibは ``Finset.sum s f`` という式を定義しています．ここで ``s : Finset α`` は ``α`` 型の要素の有限集合であり， ``f`` は ``α`` で定義される関数です． ``f`` の余域は零要素を持つ可換で結合的な加法をサポートする任意の型です． ``Algebra.BigOperators.Basic`` をインポートして ``open BigOperators`` コマンドを実行することで，より示唆的な表記である ``∑ x in s, f x`` を使うことができます．もちろん有限積についても同様の操作と表記法があります．
 
 TEXT. -/
 /- OMIT:
@@ -237,7 +237,7 @@ of ``Finset.range n``, which is the finite set of natural numbers
 less than ``n``.
 OMIT. -/
 /- TEXT:
-この ``Finset`` 型とそれがサポートする演算については次の節で説明します。現時点では ``n`` より小さい自然数の有限集合である ``Finset.range n`` のみ取り扱います。
+この ``Finset`` 型とそれがサポートする演算については次の節で説明します．現時点では ``n`` より小さい自然数の有限集合である ``Finset.range n`` のみ取り扱います．
 BOTH: -/
 section
 
@@ -272,7 +272,7 @@ provide a recursive description summation up to :math:`n`,
 and similarly for products.
 OMIT. -/
 /- TEXT:
-``Finset.sum_range_zero`` と ``Finset.sum_range_succ`` は、 :math:`n` までの総和を再帰的に記述するもので、積についても同様のものがあります。
+``Finset.sum_range_zero`` と ``Finset.sum_range_succ`` は， :math:`n` までの総和を再帰的に記述するもので，積についても同様のものがあります．
 EXAMPLES: -/
 -- QUOTE:
 example (f : ℕ → ℕ) : (∑ x in range 0, f x) = 0 :=
@@ -294,14 +294,14 @@ you can replace the proofs by ``rfl``.
 
 OMIT. -/
 /- TEXT:
-各ペアの1つ目の等式については定義的に成り立ちます。つまり証明を ``rfl`` で置き換えることができます。
+各ペアの1つ目の等式は定義上成り立ちます．つまり証明を ``rfl`` で置き換えることができます．
 
 TEXT. -/
 /- OMIT:
 The following expresses the factorial function that we defined as a product.
 OMIT. -/
 /- TEXT:
-以下は先程定義した階乗関数を積として表現したものになります。
+以下は先程定義した階乗関数を積として表現したものになります．
 EXAMPLES: -/
 -- QUOTE:
 example (n : ℕ) : fac n = ∏ i in range n, (i + 1) := by
@@ -324,7 +324,7 @@ manages to identify products that are the same up to the
 placement of parentheses and ordering of variables.
 OMIT. -/
 /- TEXT:
-簡約化のルールとして ``mul_comm`` がふくまれていることはコメントに値します。 ``x * y = y * x`` を簡約しようとすると普通は無限にループしてしまうことになるため、簡約化に追加するのは危険だと思われるでしょう。Leanの簡約化はその点を十分に認識しており、ある程度固定した上で任意の順序で項を並べたときに結果として得られる項の値が小さくなる場合にのみこの規則を適用するようにしています。次の例では ``mul_asso`` 、 ``mul_comm`` 、 ``mul_left_comm`` の3つのルールを使って簡約すると括弧の位置や変数の順序まで同じようにできることを示しています。
+特筆すべきは単純化のルールとして ``mul_comm`` がふくまれていることです．等式 ``x * y = y * x`` を使って単純化しようとすると無限ループに陥ってしまうように思われるため，この等式で単純化するのは危険だと思われるでしょう．Leanの ``simp`` はその点を十分に認識しており，ある程度固定した上で任意の順序で項を並べたときに結果として得られる項の値が小さくなる場合にのみこの規則を適用するようにしています．次の例では ``mul_asso`` ， ``mul_comm`` ， ``mul_left_comm`` の3つのルールを使って簡約すると括弧の位置や変数の順序まで同じようにできることを示しています．
 EXAMPLES: -/
 -- QUOTE:
 example (a b c d e f : ℕ) : a * (b * c * f * (d * e)) = d * (a * f * e) * (c * b) := by
@@ -339,7 +339,7 @@ rules, and the corresponding rules for addition, is a handy trick.
 
 OMIT. -/
 /- TEXT:
-大まかには、括弧を右に押し出し、両辺の式が同じ標準的な順序になるまで並べ替えることでこのルールは機能します。これらのルールとそれに対応する加法のルールを使って簡約するのは便利なテクニックです。
+大まかには，括弧を右に押し出し，両辺の式が同じ標準的な順序になるまで並べ替えることでこのルールは機能します．これらのルールとそれに対応する加法のルールを使って単純化するのは便利なテクニックです．
 
 TEXT. -/
 /- OMIT:
@@ -352,7 +352,7 @@ because calculations with division generally have side conditions.
 (It is similarly useful to avoid using subtraction on the natural numbers when possible.)
 OMIT. -/
 /- TEXT:
-和の恒等式の話に戻ると、 :math:`n` までの自然数の和が :math:`n (n + 1) / 2` となることの証明は次のようになります。証明の最初のステップでは、まず分母を消去します。これは恒等式を形式化する際に一般的に便利です。なぜなら除算は一般的に副次的な条件を持つからです。（同様に可能な限り自然数に対して減算を使わないようにすることも有効です。）
+和の等式の話に戻ると， :math:`n` までの自然数の和が :math:`n (n + 1) / 2` となることの証明は次のようになります．証明の最初のステップでは，まず分母を消去します．これは等式を形式化する際に一般的に便利です．なぜなら除算は一般的に副次的な条件を持つからです．（同様に可能な限り自然数に対して減算を使わないようにすることも有効です．）
 EXAMPLES: -/
 -- QUOTE:
 theorem sum_id (n : ℕ) : (∑ i in range (n + 1), i) = n * (n + 1) / 2 := by
@@ -368,7 +368,7 @@ We encourage you to prove the analogous identity for sums of squares,
 and other identities you can find on the web.
 OMIT. -/
 /- TEXT:
-ここで上記に似ている平方の和やググって出てくる様々な等式を証明することをお勧めします。
+ここで上記に似ている平方の和やググって出てくる様々な等式を証明することをお勧めします．
 BOTH: -/
 -- QUOTE:
 theorem sum_sqr (n : ℕ) : (∑ i in range (n + 1), i ^ 2) = n * (n + 1) * (2 * n + 1) / 6 := by
@@ -403,7 +403,7 @@ the same as that for ``Nat``).
 
 OMIT. -/
 /- TEXT:
-Leanのコアのライブラリでは、加法と乗法は再帰定義を使って定義されており、それらについての基本的な性質は帰納法を使って確立されています。もしこのような基礎的な話題に興味があるなら、乗法と加法の可換性と結合性、乗法と加法の分配性の証明に取り組むのも楽しいでしょう。これを自然数のコピーを用いて行うには以下のような流れになります。ここで ``MyNat`` で ``induction`` を使うことができることに注意してください。Leanは賢いので関連する帰納法の原理（もちろん ``Nat`` の原理と同じです）が使えることを知っています。
+Leanのコアのライブラリでは，加法と乗法は再帰定義を使って定義されており，それらについての基本的な性質は帰納法を使って確立されています．もしこのような基礎的な話題に興味があるなら，乗法と加法の可換性と結合性，乗法と加法の分配性の証明に取り組むのも楽しいでしょう．これを自前定義の自然数を用いて行うには以下のような流れになります．ここで ``MyNat`` で ``induction`` を使うことができることに注意してください．Leanは賢いので関連する帰納法の原理（もちろん ``Nat`` の原理と同じです）が使えることを知っています．
 
 TEXT. -/
 /- OMIT:
@@ -417,7 +417,7 @@ of associativity.
 
 OMIT. -/
 /- TEXT:
-まずは加法の可換性から初めましょう。経験的には加法と乗法は第二引数に対する再帰によって定義されるので、その位置に現れる変数に対する帰納法によって証明を行うのが一般的に有利です。これに対し結合則の証明でどの変数で再帰するか決めるのは少し難しいです。
+まずは加法の可換性から初めましょう．経験的には加法と乗法は第二引数に対する再帰によって定義されるので，その位置に現れる変数に対する帰納法によって証明を行うのが一般的に有利です．これに対し結合則の証明でどの変数で再帰するか決めるのは少し難しいです．
 
 TEXT. -/
 /- OMIT:
@@ -433,7 +433,7 @@ for example, is ``MyNat.add``.
 
 OMIT. -/
 /- TEXT:
-0、1、加法、乗法の通常の表記を使わずに証明を進めていくのは混乱を起こすことがあります。こういう場合に記法を定義するやり方がありますが、それは後ほど学びます。名前空間 ``MyNat`` で作業するということは、 ``MyNat.zero`` や ``MyNat.succ`` ではなく、 ``zero`` や ``succ`` と書くことができるということであり、これらの名前の解釈が他のものより優先されることを意味します。名前空間の外では、例えば以下で定義する ``add`` の完全な名前は ``MyNat.add`` となります。
+0，1，加法，乗法の通常の表記を使わずに証明を進めていくのは混乱を起こすことがあります．こういう場合に記法を定義するやり方がありますが，それは後ほど学びます．名前空間 ``MyNat`` で作業するということは， ``MyNat.zero`` や ``MyNat.succ`` ではなく， ``zero`` や ``succ`` と書くことができるということであり，これらの名前の解釈が他のものより優先されることを意味します．名前空間の外では，例えば以下で定義する ``add`` の完全な名前は ``MyNat.add`` となります．
 
 TEXT. -/
 /- OMIT:
@@ -446,7 +446,7 @@ that subtracts one from any nonzero number and fixes zero.
 The function ``pred`` can be defined by a simple instance of recursion.
 OMIT. -/
 /- TEXT:
-もしこれらを考えることが本当に *楽しい* と感じたら、切り捨てる引き算と指数を定義し、それらの性質をいくつか証明してみましょう。切り捨て減算はゼロで切り捨てる計算方法です。これを定義するためには0ではない数から1を引き、0を固定する先行関数 ``pred`` を定義するのが便利です。関数 ``pred`` は単純な再帰のインスタンスで定義できます。
+もしこれらを考えることが本当に *楽しい* と感じたら，切り捨てる引き算と指数を定義し，それらの性質をいくつか証明してみましょう．切り捨て減算はゼロで切り捨てる計算方法です．これを定義するためには0ではない数から1を引き，0を固定する先行関数 ``pred`` を定義するのが便利です．関数 ``pred`` は単純な再帰のインスタンスで定義できます．
 BOTH: -/
 -- QUOTE:
 inductive MyNat
